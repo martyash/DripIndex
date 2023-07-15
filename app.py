ec8451d8694b5f62707f53ce5783714476055ad6
Murtaza <m.ashraf@unswalumni.com>
2023-07-15 22:51:12 +1000

    First attempt to connect to Firestore

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
 import streamlit as st
+from google.cloud import firestore
+from google.oauth2 import service_account
+import json
+
+key_dict = json.loads(st.secrets(["textkey"]))
+creds = service_account.Credentials.from_service_account_info(key_dict)
+db = firestore.Client(credentials=creds, project="DripIndex")
+
+
 
 st.title(':fire: DRIP INDEX')
 st.header('Discover New Streetwear startup brands')
